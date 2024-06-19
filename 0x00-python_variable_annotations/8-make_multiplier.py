d65b4d1added4f1dda8c06b0fb94bd451304e4c2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-06-19 13:28:04 +0300

    06-19 13:28

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,8 +6,5 @@ import typing
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     ''' takes a float multiplier as argument and returns a function
     that multiplies a float by multiplier '''
-    
+    return lambda x: x * multiplier 
 
-    def fun (f: float) -> float:
-        return multiplier * f
-    return fun
